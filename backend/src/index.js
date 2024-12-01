8fe284c5e5b367a38073d759490e300c3beeacfa
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-01 17:05:58 +0500

    nit

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://realtime-chatting-app-eta.vercel.app/",
+    origin: "https://realtime-chatting-app-eta.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
