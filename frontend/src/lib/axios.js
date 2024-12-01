a69549dc3a5bf18c1dc776c9e6f7db97064cd996
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-01 14:05:55 +0500

    h

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "/api",
+    import.meta.env.MODE === "development" ? process.env.BASE_URL : "/api",
   withCredentials: true,
 });
 
